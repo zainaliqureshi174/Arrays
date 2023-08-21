c712b3ec1a4cb66a4a270f2597e2ef966b34b128
Muhammad Zain Ali <111534311+zainaliqureshi174@users.noreply.github.com>
2023-08-21 21:34:59 +0500

    Add files via upload

---
+++ Add_Matrices.cpp
@@ -0,0 +1,65 @@
+//This program add two matrices
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int m = 0, n = 0; 
+
+cout <<"Enter Order of 2 matrices (m x n):\n";
+cin >> m >> n;
+int matrix1[m][n], matrix2[m][n];
+
+cout <<"Enter the entries of matrix 1:\n";
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cin >> matrix1[i][j];
+	}
+}
+
+cout <<"Enter the entries of matrix 2:\n";
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cin >> matrix2[i][j];
+	}
+}
+
+
+cout <<"Your matrix 1 is:\n";
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cout << matrix1[i][j] << " ";
+	}
+	cout << endl;
+}
+
+
+cout <<"Your matrix 2 is:\n";
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cout << matrix2[i][j] << " ";
+	}
+	cout << endl;
+}
+
+
+cout <<"Sum of matrix 1 and matrix 2 is:\n";
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cout << matrix1[i][j] + matrix2[i][j] << " ";
+	}
+	cout << endl;
+}
+return 0;
+}
